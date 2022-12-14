af6726a709a87d4c91bfeb886147f451d9abf2db
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2022-12-14 03:09:22 +0200

    - Added show more button feature.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "boxicons"
 import "./styles/main.scss"
 // Load all the JS modules.
 import Menu from "./scripts/modules/Menu"
+import Products from "./scripts/modules/Products"
 
 // Calling All The classes.
 
 new Menu()
+new Products()
